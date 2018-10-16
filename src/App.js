e2ae9fca8f3d80cff9aa3d53dd22e3391430b8dd
cemerson <cemerson@i3logix.com>
2018-10-16 11:32:39 -0600

    Final version of basic app
    Signed-off-by: cemerson <cemerson@i3logix.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            name: 'reader'
+        }
+    }
+
+    handleNameChange = (event) => {
+        const value = event.target.value;
+        this.setState({
+            name: value
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, {this.state.name}!</h1>
+                <input
+                    type={'text'}
+                    placeholder={'Your name here'}
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
